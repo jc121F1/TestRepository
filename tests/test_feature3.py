a264aee9660c7b18cd0bbdaa9e37919169e9ee8a
jc121f1 <jackcurtis121@gmail.com>
2023-03-31 13:01:37 +0100

    Add feature 3

---
+++ tests/test_feature3.py
@@ -0,0 +1,14 @@
+import pytest
+import time
+from feature3 import Feature3
+
+class Feature3Tests():
+
+    def test_feature3method1(self):
+        assert Feature3.feature3method1() == True
+
+    def test_feature3method2(self):
+        assert Feature3.feature3method2() == True
+
+    def test_feature3method3():
+        assert Feature3.feature3method3() == True
